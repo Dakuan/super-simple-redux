ab1d224cda50cf68308dcdfd292778ad983ad0aa
Dakuan <dom.barker808@gmail.com>
2015-09-11 13:15:41 +0100

    spaces

--- src/store.js
+++ src/store.js
@@ -2,5 +2,5 @@ import { createStore } from "redux";
 import reducer from "./reducer";
 
 export default (data = {count: 0}) => {
-	return createStore(reducer, data);
+  return createStore(reducer, data);
 };
