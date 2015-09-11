b1afb6b4c7c7ebd965355ec06c0363cf8deb71eb
Dakuan <dom.barker808@gmail.com>
2015-09-11 13:14:50 +0100

    space

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,13 @@
 export default (state = {}, action) => {
-	const newState = Object.assign({}, state);
-	switch(action.type) {
-		case "INCREMENT_COUNTER" :
-			newState.count++;
-			return newState;
-		case "DECREMENT_COUNTER" :
-			newState.count--;
-			return newState;
-		default:
-			return newState;
-	}
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+    case "INCREMENT_COUNTER":
+      newState.count++;
+      return newState;
+    case "DECREMENT_COUNTER":
+      newState.count--;
+      return newState;
+    default:
+      return newState;
+  }
 };
